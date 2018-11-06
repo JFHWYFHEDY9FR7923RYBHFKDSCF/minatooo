5c742f1a0ea5694b33e360f62dc0843d6d0b563c
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-06 16:16:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -611,50 +611,48 @@ client.on('ready', function(){ // Leaked by [ @Fr3on Gamer#9338 ]
 
 
 
-client.on('message', message => { // Leaked by [ @Fr3on Gamer#9338 ]
- 	var prefix = "-";
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return
-message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-const args = message.content.split(" ").slice(1).join(" ")
-const BcList = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.setAuthor(`محتوى الرساله : ${args}`)
-.setDescription(`**برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست**`)
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
-msg.react('📝')
-.then(() => msg.react('✏'))
-.then(() =>msg.react('📝'))
-
-var EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
-var NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
-
-var EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
-var NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
-
-
-EmbedBc.on("collect", r => {
-
-message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-var EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-var bc = new
-Discord.RichEmbed()
-.setColor('RANDOM')
-.setDescription(EmbedRep)
-.setThumbnail(message.author.avatarURL)
-m.send({ embed: bc })
-msg.delete();
-})
-})
-NormalBc.on("collect", r => {
-  message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
-message.guild.members.forEach(m => {
-var NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
-m.send(NormalRep);
-msg.delete();
+client.on('message', message => {
+ 	    var prefix = "-";
+               if(!message.channel.guild) return;
+     if(message.content.startsWith(prefix + 'bc')) {
+     if(!message.channel.guild) return message.channel.send('**This Command Only For Servers**').then(m => m.delete(5000));
+   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Dont Have perms** `ADMINISTRATOR`' );
+     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+     let copy = "Speed Bot";
+     let request = `Requested By ${message.author.username}`;
+     if (!args) return message.reply('**Write Some Things To Broadcast**');message.channel.send(`**Are You Sure \nThe Broadcast: ** \` ${args}\``).then(msg => {
+     msg.react('✅')
+     .then(() => msg.react('❌'))
+     .then(() =>msg.react('✅'))
+     
+     let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+     let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+     
+     let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+     let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+  reaction1.on("collect", r => {
+     message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent To __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
+     message.guild.members.forEach(m => {
+   
+   var bc = new
+        Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTitle('Broadcast')
+        .addField('Server', message.guild.name)
+        .addField('Sender', message.author.username)
+        .addField('Message', args)
+        .setThumbnail(message.author.avatarURL)
+        .setFooter(copy, client.user.avatarURL);
+     m.send({ embed: bc })
+     msg.delete();
+     })
+     })
+     reaction2.on("collect", r => {
+     message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+     msg.delete();
+     })
+     })
+     }
      });
 
  let points = {};
